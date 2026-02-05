5d3c6e1a479611eb02cb3accb7c2bec85d5ba121
Martin Zivojinovic <zivomartin@gmail.com>
2026-02-05 10:33:19 +0100

    feat: Introduce a minimal web server listening on webhook, closes #5.

--- src/main/java/dd2480/ciserver/App.java
+++ src/main/java/dd2480/ciserver/App.java
@@ -1,10 +1,7 @@
 package dd2480.ciserver;
 
-/**
- * Hello world!
- */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+		Server.listen(8080);
     }
 }
