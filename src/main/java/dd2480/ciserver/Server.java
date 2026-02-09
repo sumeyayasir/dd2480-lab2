d10fd45049bd69019ff97267ea2a7cdb191f6fe1
sumeyayasir <124312355+sumeyayasir@users.noreply.github.com>
2026-02-09 19:50:28 +0100

    feat: connect server webhook to build processor #16
     Integrated BuildProcessor with a background thread in Server.java, and also added placeholder variables for repo URL and branch. Closes #16.

--- src/main/java/dd2480/ciserver/Server.java
+++ src/main/java/dd2480/ciserver/Server.java
@@ -9,7 +9,7 @@ import java.net.InetSocketAddress;
 
 public class Server {
 
-	
+
     public static void listen(int port) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
@@ -24,8 +24,19 @@ public class Server {
     private static void handleWebhook(HttpExchange exchange) throws IOException {
         System.out.println("Received request");
 
-        String response = "Hello from /webhook\n";
+        // 1.BuildProcessor instance
+        BuildProcessor buildProcessor = new BuildProcessor();
 
+        // 2. New thread to avoid blocking the HTTP response
+        new Thread(() -> 
+        {
+            //TEMPORARY hardcoded values
+            buildProcessor.runBuild("https://github.com/sumeyayasir/dd2480-lab2.git", "main");
+        }).start();
+
+
+        // 3. Send the response back to github
+        String response = "Hello from /webhook\n Build started!";
         exchange.sendResponseHeaders(200, response.length());
         try (OutputStream os = exchange.getResponseBody()) {
             os.write(response.getBytes());
