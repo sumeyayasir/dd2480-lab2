b210bd3c2adfced6b24ee8529800e0e64a89f6cc
Emma Lindblom <lindblom.emma01@gmail.com>
2026-02-11 08:13:24 +0100

    feat: add unit test for main method in App
    Implemented one unit test for App that creates a Method object representing main, and checks that main exists without having to start the server.
    
    Closes #48

--- src/test/java/dd2480/ciserver/AppTest.java
+++ src/test/java/dd2480/ciserver/AppTest.java
@@ -1,19 +1,21 @@
 package dd2480.ciserver;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit test for simple App.
+ * Unit test for App class.
  */
 public class AppTest {
 
     /**
-     * Rigorous Test :-)
+     * Unit test to check that the main method simply exists in the App class. Since
+     * the purpose of the main is to invoke the server by calling listen, there is
+     * not much to test that is of value.
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testMainExists() throws NoSuchMethodException {
+        var main = App.class.getMethod("main", String[].class);
+        assertNotNull(main);
     }
 }
